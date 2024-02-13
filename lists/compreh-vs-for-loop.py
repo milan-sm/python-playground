b7cebeee90cba5df26a6e24e5e33687c4c412e1e
Milan S <157825686+milan-sm@users.noreply.github.com>
2024-02-13 02:24:50 +0100

    Create compreh-vs-for-loop.py

---
+++ lists/compreh-vs-for-loop.py
@@ -0,0 +1,12 @@
+rezultat = [x for x in range(0, 11) if x % 2 == 0]
+print(rezultat)
+
+a = []
+for x in range(0, 11):
+    if x % 2 == 0:
+        a.append(x)
+
+print(a)
+
+# [0, 2, 4, 6, 8, 10]
+# [0, 2, 4, 6, 8, 10]
